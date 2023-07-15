1f3c9c002d9941612e709fd0669593f0d43e51c5
Guilherme Sales <guilherme.sales@inf.ufrgs.br>
2023-07-15 15:59:32 -0300

    Find interface name

--- sleep_server.cpp
+++ sleep_server.cpp
@@ -7,6 +7,7 @@
 #include <linux/if.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <ifaddrs.h>
 
 // Subservices
 #include "include/discovery_subservice.h"
@@ -21,12 +22,19 @@
 struct station_entry hostTable;
 
 int main(int argc, const char *argv[]) {
+    std::cout << "Initiating Sleep Server" << std::endl;
 
     auto *station = new Station();
     if (argv[1] != NULL)
-        station->init(argv[1]);    
-    station->extractMacAddress();
+        station->init(argv[1]);
+
     gethostname(station->hostname, HOST_NAME_MAX);
+    if (station->type == StationType::MANAGER)
+        std::cout << "MANAGER Station " << station->hostname << std::endl;
+    else
+        std::cout << "PARTICIPANT Station " << station->hostname << std::endl;
+
+    station->extractMacAddress();
 
     if (station->type == MANAGER)
     {
@@ -57,12 +65,29 @@ void Station::init(std::string arg)
 
 void Station::extractMacAddress() 
 {
+    std::string interface_name = "eth0";
+
+    struct ifaddrs *addrs;
+    getifaddrs(&addrs);
+    for (struct ifaddrs *addr = addrs; addr != nullptr; addr = addr->ifa_next) 
+    {
+        if (addr->ifa_addr && addr->ifa_addr->sa_family == AF_PACKET) 
+        {
+            if (strncmp("en", addr->ifa_name, 2) == 0 || strncmp("eth", addr->ifa_name, 3) == 0)
+            {
+                interface_name = addr->ifa_name;
+                break;
+            }
+        }
+    }
+    freeifaddrs(addrs);
+
 	struct ifreq ifr;
 	
 	int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
 	ifr.ifr_addr.sa_family = AF_INET;
-	strncpy((char *)ifr.ifr_name, "eth0", IFNAMSIZ-1);
+	strncpy((char *)ifr.ifr_name, interface_name.c_str(), IFNAMSIZ-1);
 
 	ioctl(fd, SIOCGIFHWADDR, &ifr);
 
@@ -71,4 +96,6 @@ void Station::extractMacAddress()
 	unsigned char* mac = (unsigned char *)ifr.ifr_addr.sa_data;
 	
 	sprintf(this->macAddress, (const char *)"%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+    std::cout << "Interface Name: " << interface_name << "\tMAC Address: " << this->macAddress << std::endl;
 }
